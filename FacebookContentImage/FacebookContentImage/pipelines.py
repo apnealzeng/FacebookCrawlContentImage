32e03a6667447d39b1f41cb8194e9707312ee935
Neal <neal.zeng@asiapac.com.cn>
2022-12-16 10:45:56 +0800

    补齐贴文图片爬虫

---
+++ FacebookContentImage/FacebookContentImage/pipelines.py
@@ -0,0 +1,144 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+import logging
+
+import pymongo
+import scrapy
+from itemadapter import ItemAdapter
+from scrapy.pipelines.images import ImagesPipeline
+
+from FacebookContentImage.items import FacebookcontentimageItem
+from FacebookContentImage.settings import MONGO_HOST, MONGO_PORT, MONGO_USERNAME, MONGO_PASSWORD, MONGO_SET_2
+
+
+class FacebookcontentimagePipeline:
+    def process_item(self, item, spider):
+        return item
+
+
+class FacebookMongoDB(scrapy.Item):
+    def open_spider(self, spider):
+        self.conn = pymongo.MongoClient(
+            MONGO_HOST,
+            MONGO_PORT
+        )
+        # self.db = self.conn[MONGO_DB]
+        # self.myset = self.db[MONGO_SET]
+
+    def process_item(self, item, spider):
+        print("--------", item.keys())
+        if item['col'] in ['kooler_kol_list', 'kooler_post_list']:
+            db_name = 'kooler_'
+            db = self.conn['kooler_buffer']
+
+        elif item['col'] in ['brand_kol_list']:
+            db_name = 'brand_'
+            db = self.conn['brand_buffer']
+        str_dict = {
+            # "_id": item["object_id"],
+            "type": db_name + item["type"],
+            "channel": item["channel"],
+            "url": item["url"],
+            "publish_time": item["publish_time"],
+            "platform_id": item["platform_id"],
+            "crawl_time": item["crawl_time"],
+            "store_time": item["store_time"],
+            "crawl_time_log": item["crawl_time_log"],
+            "store_time_log": item["store_time_log"],
+            "data": item["data"],
+            "task_id": item['task_id']
+        }
+
+        db.authenticate(MONGO_USERNAME, MONGO_PASSWORD, source='admin', mechanism='SCRAM-SHA-256')
+
+        myset = db[MONGO_SET_2]
+
+        myset.insert_one(str_dict)
+
+        return item
+
+
+class ImageDownload(ImagesPipeline):
+    headers = {
+        # "Proxy-Authorization": xun_proxy()['auth'],
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7"
+    }
+
+    def get_media_requests(self, item, info):
+        if isinstance(item, FacebookcontentimageItem):
+            if item['col'] in ['kooler_kol_list', 'kooler_post_list']:
+                bucket_name = 'kooler_buffer'
+
+            elif item['col'] in ['brand_kol_list']:
+                bucket_name = 'brand_buffer'
+
+            else:
+                raise ValueError("pipeline image dowload bucket_name")
+
+            if item.get("head_img_info"):
+
+                yield scrapy.Request(item["head_img"]["url"], headers=self.headers, meta={
+                    "user_id": item["user_id"], "img_type": "head_img", 'bucket_name': bucket_name,
+                    "platform_id": item["platform_id"], "kol_account_id": item["kol_main_id"],
+                    "task_id": item["task_id"]
+                })
+
+            elif item['data'].get("image_info"):
+
+                img_lst = item['data']["image_info"]
+
+                for img_i in img_lst:
+                    img_id = img_i["id"]
+                    photo_url_2 = img_i["url"]
+
+                    if "http" not in photo_url_2:
+                        photo_url_2 = "https:" + photo_url_2
+
+                    if photo_url_2:
+
+                        print('img_url--ddd->', photo_url_2)
+                        yield scrapy.Request(photo_url_2, headers=self.headers, meta={
+                            "img_id": img_id, "content_id": item['data']["content_id"],
+                            "img_type": "content_img", "bucket_name": bucket_name,
+                            "platform_id": item["platform_id"], "kol_account_id": item["kol_main_id"],
+                            "task_id": item["task_id"], "kol_main_id": item["kol_main_id"]
+                        })
+                    else:
+                        with open("image_error.txt", 'a') as f:
+                            f.write('content_image' + photo_url_2 + '\n')
+
+    def file_path(self, request, response=None, info=None):
+        if request.meta["img_type"] == "head_img":
+            user_id = request.meta["user_id"]
+            brand_name = request.meta["user_id"].replace('/', '_')
+            url = request.url
+
+            gcp_path = request.meta['bucket_name'] + '/facebook' + '/head_img' + '/%s' % str(user_id) + '/%s' % str(
+                brand_name) + '.jpg'
+
+            logging.info(gcp_path)
+
+            return gcp_path
+
+        elif request.meta["img_type"] == "content_img":
+            content_id = request.meta["content_id"]
+            img_id = request.meta["img_id"]
+
+            gcp_path = request.meta['bucket_name'] + '/facebook/' + 'content/img' + '/%s' % str(
+                content_id) + '/%s' % str(img_id) + '.jpg'
+
+            logging.info(gcp_path)
+
+            return gcp_path
+
+    def item_completed(self, results, item, info):
+        return item
+
+
---
+++ FacebookContentImage/FacebookContentImage/spiders/facebookcontentimage.py
@@ -0,0 +1,159 @@
+import datetime
+import re
+from urllib import parse
+
+import pymongo
+import pytz
+import scrapy
+from lxml import etree
+
+from FacebookContentImage.items import FacebookcontentimageItem
+from FacebookContentImage.settings import MONGO_HOST, MONGO_USERNAME, MONGO_PASSWORD, MONGO_PORT, IMAGES_STORE
+
+
+class FacebookcontentimageSpider(scrapy.Spider):
+    name = 'facebookcontentimage'
+    allowed_domains = ['facebook.com']
+    # start_urls = ['http://facebook.com/']
+
+    def start_requests(self):
+        conn = f'mongodb://{parse.quote_plus(MONGO_USERNAME)}:{parse.quote_plus(MONGO_PASSWORD)}@{MONGO_HOST}:{MONGO_PORT}/?authSource=admin&authMechanism=SCRAM-SHA-256'
+        client = pymongo.MongoClient(conn)
+
+        data_lst = client["potential_buffer"]["facebook_image"].find({"$regex": "story_fbid="})
+
+        for data in data_lst:
+
+            # content_url = "https://www.facebook.com/permalink.php?story_fbid=2787143381295641&id=100000000202403&substory_index=0"
+            # content_url = "https://www.facebook.com/permalink.php?story_fbid=4572263482783613&id=100000000202403"
+
+            # content_url = "https://www.facebook.com/permalink.php?story_fbid=2646752812001366&id=100000000202403"
+
+            # content_url = "https://www.facebook.com/permalink.php?story_fbid=4727126483963978&id=100000000202403"
+            content_url = data["url"]
+
+            headers = {
+                'authority': 'www.facebook.com',
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'accept-language': 'zh-CN,zh;q=0.9',
+                'cache-control': 'no-cache',
+                'cookie': 'fr=04fG66KDD9PucCG48..BjmszE.nC.AAA.0.0.BjmszE.AWW5K2HgANg; sb=xMyaYxwbJxbKZMVK3SejXLbl; datr=xMyaY9jXBO3FXFImEHdQtZXx; wd=1009x969; fr=04fG66KDD9PucCG48..BjmszE.nC.AAA.0.0.Bjms1s.AWX8qZKQ-Jo',
+                'pragma': 'no-cache',
+                'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'document',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'none',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': '1',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+            }
+
+            yield scrapy.Request(content_url, headers=headers, callback=self.parse_url, meta={
+                "post_data": data
+            })
+
+    @staticmethod
+    def parse_url(response):
+        item = FacebookcontentimageItem()
+        # image_url = response.xpath("//meta[@property='og:image']/@content").extract()
+        #
+        # print(image_url)
+
+        # 寻找贴文是否有图片
+        content_text = response.xpath("//div[@class='_4-u2 mbm _4mrt _5v3q _7cqq _4-u8']").extract()
+
+        # 贴文时间戳
+        publish_time = response.xpath("//abbr/@data-utime")
+
+        html_obj = etree.HTML(content_text[0])
+        image_url_lst = html_obj.xpath("//a[@rel='theater']/@data-ploi")
+        print(image_url_lst, len(image_url_lst))
+
+        if image_url_lst:
+
+            beijing = pytz.timezone('Asia/Shanghai')
+            date_1 = datetime.datetime.utcnow().replace(microsecond=0) + datetime.timedelta(hours=8)
+            image_info_lst = list()
+
+            if "potential" in response.meta["post_data"]["type"]:
+                bucket_name = "potential_buffer"
+            elif "kooler" in response.meta["post_data"]["type"]:
+                bucket_name = "kooler_buffer"
+            elif "brand" in response.meta["post_data"]["type"]:
+                bucket_name = "brand_buffer"
+            else:
+                raise ValueError("spider parse_url bucket_name")
+
+            for image in image_url_lst:
+                content_img_id = re.findall(r"_(.*?)_", image)[0]
+                content_img_url = image
+                image_path = IMAGES_STORE + bucket_name + '/facebook/' + 'content/img' + '/%s' % str(response.meta["post_data"]["data"]["content_id"]) + '/%s' % str(content_img_id) + '.jpg'
+                image_info_lst.append(
+                    {
+                        "id": content_img_id,
+                        "url": content_img_url,
+                        "path": image_path
+                    }
+                )
+            try:
+                time1 = datetime.datetime.utcfromtimestamp(int(publish_time)).replace(microsecond=0) + datetime.timedelta(hours=8)
+                p_time = time1.astimezone(beijing).isoformat()
+            except Exception as e:
+                p_time = ""
+
+            crawl_times = date_1.astimezone(beijing).isoformat()
+            item["type"] = response.meta["post_data"]["type"]
+            item["channel"] = "facebook"
+            item["url"] = response.url
+            item["publish_time"] = p_time
+            item["platform_id"] = response.meta["post_data"]["platform_id"]
+            item["crawl_time"] = crawl_times
+            item["store_time"] = crawl_times
+            item["crawl_time_log"] = [
+                crawl_times
+            ]
+            item["store_time_log"] = [
+                crawl_times
+            ]
+            brand_mention_str_lst = re.findall(r'@(\w+)', response.meta["post_data"]["data"]["content"])
+
+            if brand_mention_str_lst:
+                brand_mention = [
+                    {
+                        "type": "string", "value": brand_mention_str_lst
+                    },
+                    {
+                        "type": "link", "value": []
+                    },
+                    {
+                        "type": "img", "value": []
+                    }
+                ]
+            else:
+                brand_mention = []
+
+            item["data"] = {
+                "content_id": response.meta["post_data"]["data"]["content_id"],
+                "homepage_url": "",
+                "hash_tag": re.findall(r'#(\w+)', response.meta["post_data"]["data"]["content"]),
+                "lang": "",
+                "title": "",
+                "sub_title": "",
+                "content": response.meta["post_data"]["data"]["content"],
+                "other_content": response.meta["post_data"]["data"]["other_content"],
+                "brand_mention": brand_mention,
+                "content_level": "0",
+                "share_content": response.meta["post_data"]["data"]["share_content"],
+                "image_info": image_info_lst,
+                "video_info": response.meta["post_data"]["data"]["video_info"],
+                "sn_interact_num": response.meta["post_data"]["data"]["sn_interact_num"]
+            }
+
+            item['job_uid'] = response.meta['job_uid']
+            item['col'] = response.meta['col']
+            item['kooler_ref_id'] = response.meta['kooler_ref_id']
+
+            yield item
+
